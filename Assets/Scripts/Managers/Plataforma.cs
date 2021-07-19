8d0554b1b6ce5288850fa0569e8890b9bfe2726c
Francisco Carmona <fran_9824@hotmail.com>
2021-07-19 12:25:48 +0200

    Primera version de la generacion de escenario

---
+++ Assets/Scripts/Managers/Plataforma.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // aqui va a haber que poner una forma de desplazarlo y que combine bien, de momento lo movere tal cual a ver
+        transform.Translate(Vector3.left * Time.deltaTime* speed);
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("KillPlane"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
